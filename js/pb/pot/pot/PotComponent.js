8a0dddb95b9db4ac31f901cb461435af277c444e
dashersw <armagan@amcalar.com>
2012-08-13 21:36:25 +0300

    Added pot component for modelling potentiometers.

---
+++ js/pb/pot/pot/PotComponent.js
@@ -0,0 +1,136 @@
+// Copyright 2012 Armagan Amcalar. All Rights Reserved.
+//
+// This file is part of Pedalboard.js.
+//
+// Pedalboard.js is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Pedalboard.js is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Pedalboard.js.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @fileoverview Base pot component.
+ */
+
+
+goog.provide('pb.pot.PotComponent');
+goog.require('goog.events.MouseWheelHandler');
+goog.require('pb.pot.PotComponentModel');
+goog.require('tart.ui.DlgComponent');
+
+
+
+/**
+ * Pot component models a virtual potentiometer. This base class is used to adjust audio parameter values of pedals.
+ *
+ * @constructor
+ * @extends {tart.ui.DlgComponent}
+ *
+ * @param {AudioParam} param Audio parameter this pot will adjust. Can be gain, etc.
+ * @param {string} name Name of the pot. Will be written under it.
+ * @param {number} range The multiplier of the effect. Some effects (such as gain) need this to be on the order of
+ *                       thousands.
+ */
+pb.pot.PotComponent = function(param, name, range) {
+    this.model = new this.modelClass(param, name, range || 1);
+    this.setValue(10);
+
+    goog.base(this);
+};
+goog.inherits(pb.pot.PotComponent, tart.ui.DlgComponent);
+
+
+/**
+ * @type {function(new: pb.pot.PotComponentModel)} The component model this pot component will work with.
+ */
+pb.pot.PotComponent.prototype.modelClass = pb.pot.PotComponentModel;
+
+
+/**
+ * Sets the new value for this pot's audio parameter.
+ *
+ * @param {number} newValue New value to be set.
+ */
+pb.pot.PotComponent.prototype.setValue = function(newValue) {
+    if (newValue < 0.1) newValue = 0.1;
+    this.model.setValue(newValue);
+};
+
+
+/**
+ * Updates the user interface - rotation - accordingly.
+ */
+pb.pot.PotComponent.prototype.updateUi = function() {
+    var newStyle = 'rotate(' + (this.model.getNormalizedValue() * 260) + 'deg)';
+    this.getChild(this.mappings.KNOB)[0].style['-webkit-transform'] = newStyle;
+};
+
+
+/**
+ * @override
+ */
+pb.pot.PotComponent.prototype.templates_base = function() {
+    return '<div class="pot" id="' + this.id + '">' +
+               '<div class="knob"></div>' +
+               '<div class="name">' + this.model.name + '</div>' +
+           '</div>';
+};
+
+
+/**
+ * @override
+ */
+pb.pot.PotComponent.prototype.render = function() {
+    this.updateUi();
+
+    this.rendered = true;
+};
+
+
+/**
+ * @enum {string} DOM mappings.
+ */
+pb.pot.PotComponent.prototype.mappings = {
+    KNOB: '.knob'
+};
+
+
+/**
+ * @override
+ */
+pb.pot.PotComponent.prototype.bindModelEvents = function() {
+    goog.events.listen(this.model, pb.pot.PotComponentModel.EventType.VALUE_CHANGED, function(e) {
+        console.log(e.newValue);
+        this.rendered && this.updateUi();
+    }, false, this);
+};
+
+(function(proto) {
+    proto.events = {};
+    var mousedown = proto.events[goog.events.EventType.MOUSEDOWN] = {};
+
+    mousedown[proto.mappings.KNOB] = function(e) {
+        this.flag = true;
+        this.oldY = e.clientY;
+
+        var mouseup = goog.events.listen(document.body, 'mouseup', function(e) {
+            this.flag = false;
+            goog.events.unlistenByKey(mousemove);
+            goog.events.unlistenByKey(mouseup);
+        }, false, this);
+
+        var mousemove = goog.events.listen(document.body, 'mousemove', function(e) {
+            if (this.flag) {
+                this.setValue(this.model.getNormalizedValue() * 10 - (e.clientY - this.oldY) * 0.05);
+                this.oldY = e.clientY;
+            }
+        }, false, this);
+    };
+})(pb.pot.PotComponent.prototype);
---
+++ js/pb/pot/pot/PotComponentModel.js
@@ -0,0 +1,83 @@
+// Copyright 2012 Armagan Amcalar. All Rights Reserved.
+//
+// This file is part of Pedalboard.js.
+//
+// Pedalboard.js is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Pedalboard.js is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Pedalboard.js.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @fileoverview Base pot component model.
+ */
+
+
+goog.provide('pb.pot.PotComponentModel');
+goog.require('tart.ui.ComponentModel');
+
+
+
+/**
+ * @constructor
+ * @extends {tart.ui.ComponentModel}
+ *
+ * @param {AudioParam} param Audio parameter this pot will adjust. Can be gain, etc.
+ * @param {string} name Name of the pot. Will be written under it.
+ * @param {number} range The multiplier of the effect. Some effects (such as gain) need this to be on the order of
+ *                       thousands.
+ */
+pb.pot.PotComponentModel = function(param, name, range) {
+    goog.base(this);
+
+    this.param = param;
+    this.name = name;
+    this.range = range;
+};
+goog.inherits(pb.pot.PotComponentModel, tart.ui.ComponentModel);
+
+
+/**
+ * Sets the new value for this pot's audio parameter and lets whoever listens hear about it.
+ *
+ * @param {number} newValue New value to be set.
+ */
+pb.pot.PotComponentModel.prototype.setValue = function(newValue) {
+    var oldValue = this.param.value;
+    newValue = newValue * this.range;
+    newValue = Math.min(newValue, 10 * this.range);
+    newValue = newValue / 10;
+    if (newValue < 0)
+        return;
+    this.param.value = newValue;
+
+    this.dispatchEvent({
+        type: pb.pot.PotComponentModel.EventType.VALUE_CHANGED,
+        newValue: newValue,
+        oldValue: oldValue
+    });
+};
+
+
+/**
+ * @return {number} The normalized value of this pot's parameter (as calculated in value / range).
+ */
+pb.pot.PotComponentModel.prototype.getNormalizedValue = function() {
+    return this.param.value / this.range;
+};
+
+
+/**
+ *
+ * @enum {string}
+ */
+pb.pot.PotComponentModel.EventType = {
+    VALUE_CHANGED: 'valueChanged'
+};
