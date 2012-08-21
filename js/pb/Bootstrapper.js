72d598ca9d8fabcf9baabad830304919c4f30246
dashersw <armagan@amcalar.com>
2012-08-21 20:52:45 +0300

    Cleaned the Bootstrapper now that we have Stage and Board classes.

--- js/pb/Bootstrapper.js
+++ js/pb/Bootstrapper.js
@@ -20,16 +20,7 @@
  */
 
 goog.provide('pb.Bootstrapper');
-goog.require('goog.debug.ErrorHandler');
-goog.require('goog.events.EventHandler');
-goog.require('goog.events.EventTarget');
-goog.require('pb.box.conv.Component');
-goog.require('pb.box.overdrive.Component');
-goog.require('pb.box.reverb.Component');
-goog.require('pb.box.volume.Component');
-goog.require('pb.io.FileInput');
-goog.require('pb.io.Output');
-goog.require('pb.io.StreamInput');
+goog.require('pb.Stage');
 
 
 
@@ -38,72 +29,14 @@ goog.require('pb.io.StreamInput');
  * @constructor
  */
 pb.Bootstrapper = function() {
-    this.context = new webkitAudioContext();
-    this.init();
-    this.route();
+    this.stage = new pb.Stage();
+    var board = new pb.Board(this.stage.getContext());
+    this.stage.setBoard(board);
+
+    $('body').append(this.stage.getPlaceholder());
+    this.stage.render();
+
+    this.stage.play();
 };
 
-
-/**
- * Initializes the input, the output and the pedal components.
- */
-pb.Bootstrapper.prototype.init = function() {
-    this.input = new pb.io.FileInput(this.context, 'audio/samples/sample.mp3');
-    //        this.input = new pb.io.StreamInput(this.context);
-
-    this.output = new pb.io.Output(this.context);
-    this.overdrivePedal = new pb.box.overdrive.Component(this.context);
-    this.reverbPedal = new pb.box.reverb.Component(this.context);
-    this.volumePedal = new pb.box.volume.Component(this.context);
-    this.speaker = new pb.box.conv.Component(this.context);
-
-    this.effects = [
-        this.overdrivePedal,
-        this.reverbPedal,
-        this.volumePedal,
-        this.speaker
-    ];
-    this.chain = [].concat(this.input, this.effects, this.output);
-
-    goog.array.forEach(this.effects, function(effect) {
-        goog.dom.getElement('board').innerHTML += effect.getPlaceholder();
-    });
-    goog.array.forEach(this.effects, function(effect) {
-        effect.render();
-    });
-
-    this.overdrivePedal.setDrive(7);
-    this.volumePedal.setLevel(10);
-    this.reverbPedal.setLevel(3);
-};
-
-
-/**
- * Routes the signal.
- * Input -> volume pedal -> reverb pedal
- */
-pb.Bootstrapper.prototype.route = function() {
-    for (var i = 0; i < this.chain.length - 1; i++) {
-        this.chain[i].connect(this.chain[i + 1]);
-    }
-};
-
-
-/**
- * Plays the input.
- */
-pb.Bootstrapper.prototype.play = function() {
-    this.input.play();
-};
-
-
-/**
- * Stops the input.
- */
-pb.Bootstrapper.prototype.stop = function() {
-    this.input.stop();
-};
-
-goog.exportProperty(pb.Bootstrapper.prototype, 'play', pb.Bootstrapper.prototype.play);
-goog.exportProperty(pb.Bootstrapper.prototype, 'stop', pb.Bootstrapper.prototype.stop);
 goog.exportSymbol('pb', pb.Bootstrapper);
