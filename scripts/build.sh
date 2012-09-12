f3216e79b4bd4336b5c6efbaa7eff1337656d9b4
dashersw <armagan@amcalar.com>
2012-09-12 18:53:39 +0300

    Removed pretty printing from build.sh.

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,6 @@ js/library/tartJS/tools/goog/build/closurebuilder.py \
 --compiler_flags="--create_source_map='js/compiled/source_map.js'" \
 --compiler_flags="--property_map_output_file='js/compiled/properties.out'" \
 --compiler_flags="--variable_map_output_file='js/compiled/variables.out'" \
---compiler_flags="--formatting=PRETTY_PRINT" \
 --compiler_flags="--warning_level=VERBOSE" \
 --compiler_flags="--externs=js/library/tartJS/tart/externs/jquery-1.4.4.externs.js" \
 --compiler_flags="--externs=js/library/tartJS/tart/externs/tart.externs.js" \
