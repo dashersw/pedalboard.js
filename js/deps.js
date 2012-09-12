162468bbff6d740da2d8eff1ac4ed9931b8097d4
dashersw <armagan@amcalar.com>
2012-09-13 00:30:41 +0300

    Updated deps.js.

--- js/deps.js
+++ js/deps.js
@@ -895,14 +895,16 @@ goog.addDependency('../../../../../library/tartJS/third_party/goog/third_party/c
 goog.addDependency('../../../../../library/tartJS/third_party/goog/third_party/closure/goog/silverlight/clipboardbutton.js', ['goog.silverlight.ClipboardButton', 'goog.silverlight.ClipboardButtonType', 'goog.silverlight.ClipboardEvent', 'goog.silverlight.CopyButton', 'goog.silverlight.PasteButton', 'goog.silverlight.PasteButtonEvent'], ['goog.asserts', 'goog.events.Event', 'goog.math.Size', 'goog.silverlight', 'goog.ui.Component']);
 goog.addDependency('../../../../../library/tartJS/third_party/goog/third_party/closure/goog/silverlight/silverlight.js', ['goog.silverlight'], []);
 goog.addDependency('../../../../../library/tartJS/third_party/goog/third_party/closure/goog/silverlight/supporteduseragent.js', ['goog.silverlight.supportedUserAgent'], []);
-goog.addDependency('../../../../../pb/Board.js', ['pb.Board'], ['goog.debug.ErrorHandler', 'goog.events.EventHandler', 'goog.events.EventTarget', 'pb.box.conv.Component', 'pb.box.overdrive.Component', 'pb.box.reverb.Component', 'pb.box.volume.Component', 'tart.ui.DlgComponent']);
+goog.addDependency('../../../../../pb/Board.js', ['pb.Board'], ['goog.debug.ErrorHandler', 'goog.events.EventHandler', 'goog.events.EventTarget', 'pb.Connectable.Component', 'pb.box.conv.Component', 'pb.box.overdrive.Component', 'pb.box.reverb.Component', 'pb.box.volume.Component', 'tart.ui.DlgComponent']);
 goog.addDependency('../../../../../pb/Bootstrapper.js', ['pb.Bootstrapper'], ['pb.Stage']);
 goog.addDependency('../../../../../pb/Connectable.js', ['pb.Connectable'], []);
+goog.addDependency('../../../../../pb/Connectable/Component.js', ['pb.Connectable.Component'], ['pb.Connectable.ComponentModel', 'pb.ConnectableComponent', 'pb.footswitch.toggle.Component', 'pb.pot.Component', 'pb.shadowMaker', 'pb.ui.Component', 'tart.ui.DlgComponent']);
+goog.addDependency('../../../../../pb/Connectable/ComponentModel.js', ['pb.Connectable.ComponentModel'], ['pb.Connectable']);
 goog.addDependency('../../../../../pb/ConnectableComponent.js', ['pb.ConnectableComponent'], []);
 goog.addDependency('../../../../../pb/ShadowMaker.js', ['pb.shadowMaker'], ['goog.color', 'goog.style']);
-goog.addDependency('../../../../../pb/Stage.js', ['pb.Stage'], ['pb.Board', 'pb.io.FileInput', 'pb.io.Output', 'pb.io.StreamInput', 'tart.ui.DlgComponent']);
-goog.addDependency('../../../../../pb/box/box/Component.js', ['pb.box.box.Component'], ['pb.ConnectableComponent', 'pb.box.box.ComponentModel', 'pb.footswitch.toggle.Component', 'pb.pot.Component', 'pb.shadowMaker', 'tart.ui.DlgComponent']);
-goog.addDependency('../../../../../pb/box/box/ComponentModel.js', ['pb.box.box.ComponentModel'], ['pb.Connectable']);
+goog.addDependency('../../../../../pb/Stage.js', ['pb.Stage'], ['pb.Board', 'pb.io.FileInput', 'pb.io.Output', 'pb.io.StreamInput', 'pb.ui.Component']);
+goog.addDependency('../../../../../pb/box/box/Component.js', ['pb.box.box.Component'], ['pb.Connectable.Component', 'pb.box.box.ComponentModel', 'pb.footswitch.toggle.Component', 'pb.pot.Component', 'pb.shadowMaker']);
+goog.addDependency('../../../../../pb/box/box/ComponentModel.js', ['pb.box.box.ComponentModel'], ['pb.Connectable.ComponentModel']);
 goog.addDependency('../../../../../pb/box/conv/Component.js', ['pb.box.conv.Component'], ['pb.box.box.Component', 'pb.box.conv.ComponentModel']);
 goog.addDependency('../../../../../pb/box/conv/ComponentModel.js', ['pb.box.conv.ComponentModel'], ['pb.box.box.ComponentModel']);
 goog.addDependency('../../../../../pb/box/overdrive/Component.js', ['pb.box.overdrive.Component'], ['pb.box.box.Component', 'pb.box.overdrive.ComponentModel']);
@@ -911,7 +913,7 @@ goog.addDependency('../../../../../pb/box/reverb/Component.js', ['pb.box.reverb.
 goog.addDependency('../../../../../pb/box/reverb/ComponentModel.js', ['pb.box.reverb.ComponentModel'], ['pb.box.box.ComponentModel']);
 goog.addDependency('../../../../../pb/box/volume/Component.js', ['pb.box.volume.Component'], ['pb.box.box.Component', 'pb.box.volume.ComponentModel']);
 goog.addDependency('../../../../../pb/box/volume/ComponentModel.js', ['pb.box.volume.ComponentModel'], ['pb.box.box.ComponentModel']);
-goog.addDependency('../../../../../pb/footswitch/Component.js', ['pb.footswitch.Component'], ['goog.events.MouseWheelHandler', 'pb.footswitch.ComponentModel', 'tart.ui.DlgComponent']);
+goog.addDependency('../../../../../pb/footswitch/Component.js', ['pb.footswitch.Component'], ['goog.events.MouseWheelHandler', 'pb.footswitch.ComponentModel', 'pb.ui.Component']);
 goog.addDependency('../../../../../pb/footswitch/ComponentModel.js', ['pb.footswitch.ComponentModel'], ['tart.ui.ComponentModel']);
 goog.addDependency('../../../../../pb/footswitch/toggle/Component.js', ['pb.footswitch.toggle.Component'], ['pb.footswitch.Component']);
 goog.addDependency('../../../../../pb/footswitch/toggle/ComponentModel.js', ['pb.footswitch.toggle.ComponentModel'], ['pb.footswitch.ComponentModel']);
@@ -919,5 +921,6 @@ goog.addDependency('../../../../../pb/io/FileInput.js', ['pb.io.FileInput'], ['p
 goog.addDependency('../../../../../pb/io/Input.js', ['pb.io.Input'], ['goog.events.EventTarget', 'pb.ConnectableComponent']);
 goog.addDependency('../../../../../pb/io/Output.js', ['pb.io.Output'], ['pb.ConnectableComponent']);
 goog.addDependency('../../../../../pb/io/StreamInput.js', ['pb.io.StreamInput'], ['pb.io.Input']);
-goog.addDependency('../../../../../pb/pot/Component.js', ['pb.pot.Component'], ['pb.pot.ComponentModel', 'pb.shadowMaker', 'tart.ui.DlgComponent']);
+goog.addDependency('../../../../../pb/pot/Component.js', ['pb.pot.Component'], ['pb.pot.ComponentModel', 'pb.shadowMaker', 'pb.ui.Component']);
 goog.addDependency('../../../../../pb/pot/ComponentModel.js', ['pb.pot.ComponentModel'], ['tart.ui.ComponentModel']);
+goog.addDependency('../../../../../pb/ui/Component.js', ['pb.ui.Component'], ['goog.ui.Component', 'goog.ui.Container', 'goog.ui.Control', 'tart.dom', 'tart.ui.ComponentManager']);
